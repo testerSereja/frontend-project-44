f86dcf53ec4bd164abee503027ec1d7fb3c21e49
testerSereja <kornikov.sergey@hexly.ru>
2022-12-22 11:18:50 +0300

    added  badge

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question(`Welcome to the Brain Games!
+May I have your name? `);
+
+
+ export default name;
+//console.log(greeting())
+
+
