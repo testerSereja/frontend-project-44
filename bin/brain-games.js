f5a831deb4d619e7fb5aa13fa5564426422dfd24
testerSereja <kornikov.sergey@hexly.ru>
2023-01-24 13:23:19 +0300

    update brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
-const greeting = (name) => {
+const greeting = () => {
+   console.log('Welcome to the Brain Games!')
+   const name = readlineSync.question(`May I have your name? `);
   console.log(`Hello, ${name}!`);
 };
+greeting()
