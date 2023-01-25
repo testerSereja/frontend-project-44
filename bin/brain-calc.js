ee74dd65466bd3718cc367441e087cb50453d023
testerSereja <kornikov.sergey@hexly.ru>
2023-01-25 16:40:44 +0300

    linter3

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -8,18 +8,25 @@ function brainCalc(username) {
     What is the result of the expression?`);
   for (let i = 0; i < 3; i += 1) {
     const operator = ['+', '-', '*'];
-    const randomOperator = Math.floor(Math.random() * operator.length);
-
+    const randomOperator = operator[Math.floor(Math.random() * operator.length)];
     const num1 = randomNumber(99);
     const num2 = randomNumber(99);
-    const operation = operator[randomOperator];
-    const expression = `${num1} ${operation} ${num2}`;
-    const rightAnswer = eval(expression);
+
+    const expression = `${num1} ${randomOperator} ${num2}`;
+    let rightanswer;
+
+    if (randomOperator === '+') {
+      rightanswer = num1 + num2;
+    } else if (randomOperator === '-') {
+      rightanswer = num1 - num2;
+    } else if (randomOperator === '*') {
+      rightanswer = num1 * num2;
+    }
     console.log(`Question: ${expression}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (rightAnswer !== +answer) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.
+    if (rightanswer !== +answer) {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightanswer}.
 Let's try again, ${username}!`);
       return;
     }
