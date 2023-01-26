b53f9cce9a1274a80d8c09e98d4be0179b6c917b
testerSereja <kornikov.sergey@hexly.ru>
2023-01-26 09:50:59 +0300

    linter5

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,18 +6,19 @@ function brainPrime(username) {
   console.log(`Hello, ${username}
   Answer "yes" if given number is prime. Otherwise answer "no".`);
 
+  function primality(num) {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  }
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = (base) => Math.round(Math.random() * base);
     const num = randomNumber(99);
     console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ');
 
-    function primality(num) {
-      for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) return false;
-      }
-      return num > 1;
-    }
+    
     const rightAnswer = primality(num) ? 'yes' : 'no';
     console.log(rightAnswer);
     if (rightAnswer !== answer) {
